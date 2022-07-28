ca178dc280abf98ec6ce139b05c6449ad79f05dc
Federico <fedeprenollio@gmail.com>
2022-07-28 16:28:31 -0300

    aa

--- src/app.js
+++ src/app.js
@@ -1,23 +1,15 @@
-
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
 
 //setting
-app.set('port', process.env.PORT || 3002)
+app.set("port", process.env.PORT || 3002);
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 //routes
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://notes-inmr86wez-fedeprenollio.vercel.app/'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-})
-app.use('/api/users',require('./routes/users') );
-app.use('/api/notes',require('./routes/notes') );
 
+app.use("/api/users", require("./routes/users"));
+app.use("/api/notes", require("./routes/notes"));
 
 module.exports = app;
